9e61ef1ea99fe0fd093bb04037854a6619041e64
Wanderley Maciel <wanderley_melo@hotmail.com>
2022-10-01 10:12:07 -0300

    Primeiro commit

---
+++ 2022-10-01.js
@@ -0,0 +1,17 @@
+var distancia = 600; // numerico inteiro
+var consumo = 45.5; // numerico flutuante
+var veiculo = `moto`; // string
+var tem_passageiro = true; // booleano verdadeiro
+var tem_pet = false; // booleano falso
+
+var preco = 4.63
+var resultado = (distancia/consumo)*preco
+var quanto_posso_gastar = 0
+var limite_cartao = 500
+if (quanto_posso_gastar>=resultado) {
+
+} else if (limite_cartao>=resultado) {
+
+} else {
+    
+}
