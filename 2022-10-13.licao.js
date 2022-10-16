2fc9f6aebabfd963cbfa63bb5da0380f8ad33920
deleymaciel <114811318+deleymaciel@users.noreply.github.com>
2022-10-16 18:07:06 -0300

    Licao 2022-10-13

--- 2022-10-13.licao.js
+++ 2022-10-13.licao.js
@@ -1,5 +1,5 @@
-var banco_de_dados  [];
-function adicionaEntrega(nome_cliente, endereço, forma_de_pagamento, valor, nome do item) {
+var banco_de_dados = [];
+function adicionaEntrega(nome_cliente, endereço, forma_de_pagamento, valor, nome_do_item) {
     banco_de_dados.push({
         nome_cliente : nome_cliente,
         endereco : endereco,
@@ -9,3 +9,13 @@ function adicionaEntrega(nome_cliente, endereço, forma_de_pagamento, valor, nom
       })
 }
 
+function imprimirBancoDeDados(){
+  console.log(banco_de_dados);
+}
+
+adicionaEntrega(`Wanderley Maciel`, `Rua Araticum 344`, `Dinheiro`, 17.99, `X-Tudo`)
+adicionaEntrega(`Kamila Maciel`, `Rua Araticum 344`, `Debito`, 15.99, `X-Bacon`)
+adicionaEntrega(`Marcos Maciel`, `Rua Dos Biruleibi 22`, `Credito`, 25.99, `X-Picanha`)
+adicionaEntrega(`Rafael Maciel`, `Rua do Pc 22`, `Dinheiro`, 30.99, `X-Jamal e Refri`)
+adicionaEntrega(`Gabriel Inacio`, `Rua do Notebook 22`, `pix`, 15.99, `X-Boco`)
+adicionaEntrega(`Beth Maciel`, `Rua das Maes 123`, `Debito`, 22.99, `X-Surra`)
